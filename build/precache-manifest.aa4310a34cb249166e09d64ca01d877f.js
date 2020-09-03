731903cfec6c75ad33099f664ecaf2430a8013c7
synoratska <39731140+synoratska@users.noreply.github.com>
2020-09-03 14:22:05 +0300

    react-instagram-app
    A simple react Instagram app where registered user can upload his photos with a description and other users can leave their comments under them after logging in.
    Firebase is used as a backend for it and ReactJS as frontend.

---
+++ build/precache-manifest.aa4310a34cb249166e09d64ca01d877f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882c7f2e4252ee05091e41e31833a10d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da4e9492113831a6145f",
+    "url": "/static/css/main.384e141c.chunk.css"
+  },
+  {
+    "revision": "6bc6f10ba8f9bb4b1123",
+    "url": "/static/js/2.14477bdf.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.14477bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4e9492113831a6145f",
+    "url": "/static/js/main.6c61bec6.chunk.js"
+  },
+  {
+    "revision": "ef59cf3efbdf2dc06cda",
+    "url": "/static/js/runtime-main.aaa281d3.js"
+  }
+]);
